11657183c19df7415be7fe9d71c53ec90be3946f
haja <haja@itu.dk>
2019-11-06 20:50:37 +0100

    Added sbt build

--- src/contractLanguages/templates/ExchangeOfGoods.scala
+++
@@ -1,8 +0,0 @@
-package contractLanguages.templates
-
-case class ExchangeOfGoods()
-// contract template for the exchange of goods between 2 parties:
-
-// party A orders
-
-// party B supplies the order
