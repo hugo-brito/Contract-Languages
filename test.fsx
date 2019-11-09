4de033d3dea3ab7ba60ecfe3db23531be9d220e4
haja <haja@itu.dk>
2019-11-09 16:54:52 +0100

    added a bit to the evauator, but not done now

--- test.fsx
+++ test.fsx
@@ -8,14 +8,14 @@ type Order = {
     amount: int
     item: string
     recipient: Agent // receives the event
-    instigator: Agent // carries out the the event
+    instigator: Agent // instigates out the the event
     time: string
 }
 
 type Delivery = {
     item: string
     recipient: Agent // receives the event
-    instigator: Agent // carries out the the event
+    instigator: Agent // instigates out the the event
     time: string
 }
 
@@ -25,7 +25,18 @@ type Events =
 
 type Contract = 
     | Atom of Events
-    | Seq of Contract * Contract
+    | Then of Contract * Contract
+    | Or of Contract * Contract
+    | Where of (Events -> bool)
+    | Succ | Fail
+
+let rec evalC (e :Events) = function
+    | Atom(ord) -> Atom(ord)
+    | Then(c1,c2) -> evalC e c1
+    | Or -> 
+    | Where -> 
+    | Succ -> Succ
+    | Fail -> Fail
 
 let date = "01/11/2019"
 let item = "bike"
@@ -34,8 +45,8 @@ let buyer = {name = "Yvonne"}
 let amount = 200
 
 let templateResourceContract buyer seller amount item time : Contract = 
-    let orderEvent = Atom (Ord {amount = amount; 
-                                    item = item; 
+    let orderEvent = Atom (Ord {amount = amount;
+                                    item = item;
                                     recipient = seller;
                                     instigator = buyer;
                                     time = time})
@@ -44,9 +55,21 @@ let templateResourceContract buyer seller amount item time : Contract =
                                     recipient = buyer;
                                     instigator = seller;
                                     time = time})
-    Seq(orderEvent,delEvent)
+    Then(orderEvent,delEvent)
 
 templateResourceContract buyer seller amount item date
+
+/// There needs to happen an order event of hammer with price 10 made by agent Yvonne (buyer)
+///  which John will then received at 01/11/2019 (date).
+/// If that does not happen, the order will fail.
+/// If it is does happen then it will succeed and the contract is complete.
+Then(
+    Then(
+        Or(
+            Atom(Ord{amount=10; item="hammer"; recipient=seller; instigator=buyer;time=date}),
+            Atom(Ord{amount=10; item!="hammer"; recipient!=seller; instigator=buyer;time=date})),
+        Fail),
+    Succ)
 
 // let ole = {name = "Ole"}
 // let order = {amount = 10; agent =  ole; item = "Ost"}
