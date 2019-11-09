fc6721d4198f26cdceb152352fc18071ffcfb583
Hugo Brito <hubr@itu.dk>
2019-11-09 19:00:58 +0100

    Update Agent.scala

--- src/contractLanguages/Agent.scala
+++ src/contractLanguages/Agent.scala
@@ -6,15 +6,18 @@ package contractLanguages
 
  */
 
-sealed abstract case class Agent(name: String, taxNum: Int)
+sealed abstract case class Agent(name: String)
 // sealed: can only be extended this THIS file
 // abstract: cannot be instantiated
 // case: many out of the box functions such as hashCode, toString.. etc
 
 // the constructor takes a name and a taxNum.
 
-case class Supplier(override val name: String, override val taxNum: Int, inventory: Map[String, Int]) extends Agent(name, taxNum)
-// has an inventory of goods to sell
+case class Supplier(override val name: String, override val taxNum: Int, inventory: Map[String, Int]) extends Agent(name, taxNum) {
+	// has an inventory of goods to sell
+	def apply(name: String, taxNum: Int, inventory: Map[String, Int]): Supplier = new Supplier(agent, timeStamp, recipient, item): Supplier
+}
 
-case class Buyer(override val name: String, override val taxNum: Int, balanceDKK: Int) extends Agent(name, taxNum)
+
+case class Buyer(override val name: String) extends Agent(name, taxNum)
 // has money to pay for goods
