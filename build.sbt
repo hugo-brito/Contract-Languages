8e5cfaf8b0fc862c6ad71ace16efb5dd981dbbf5
haja <haja@itu.dk>
2019-11-28 18:18:05 +0100

    changed language so now should fulfill specs

--- build.sbt
+++ build.sbt
@@ -12,6 +12,13 @@ libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 initialCommands in console := """
 import Contracts.Language._
 import Contracts.Templates._
-import Contracts.Date._
-import Contracts.jTemplates._
+import Contracts.Dates._
+
+val buyer = Agent("Jonas")
+val seller = Agent("Hugo")
+val item = Item("bike", 3499)
+val amountPaid = 3499
+val t1 = Transaction(buyer,seller, item, Date())
+val t2 = Transaction(buyer,seller, MonetaryValue(amountPaid), Date())
+val t3 = Transaction(seller,buyer, item, Date())
 """
--- src/main/scala/Contracts/Language.scala
+++ src/main/scala/Contracts/Language.scala
@@ -15,7 +15,10 @@ object Language extends App {
     case class Invoice(seller: Agent, buyer: Agent, total: MonetaryValue, issue: Date, due: Date) extends Resource
     
     // Event type
-    case class Transaction(instigator: Agent, recipient: Agent, resource: Resource, timeStamp: Date, isAnOrder: Boolean = false, isAReturn: Boolean = false)
+    case class Transaction(instigator: Agent, recipient: Agent, resource: Resource, timeStamp: Date)
+
+    /*
+    , isAnOrder: Boolean = false, isAReturn: Boolean = false)
     object Transaction {
         def Order(instigator: Agent, recipient: Agent, resource: Resource, timeStamp: Date): Transaction =
             new Transaction(instigator, recipient, resource: Resource, timeStamp: Date, true, false)
@@ -24,17 +27,17 @@ object Language extends App {
             new Transaction(instigator, recipient, resource: Resource, timeStamp: Date, false, true)
             // This way, both the buyer and the seller can return the items and the amount that refers to the purchase
     }
+    */
+
 
     // Contract type
     sealed trait Contract {
-        def ||(that: Contract) = Or(this, that)
-        def &&(that: Contract) = Union(this, that)
-        def followedBy(that: Contract) = Seq(this, that)
+        def or(that: Contract) = Or(this, that)
+        def then(that: Contract) = Seq(this, that)
     }
-    case class Atom(val f: Transaction => Boolean) extends Contract
-    case class Seq(val c1: Contract, val c2: Contract) extends Contract 
+    case class Commitment(val f: Transaction => Option[Contract]) extends Contract
+    case class Seq(val c1: Contract, val c2: Contract) extends Contract
     case class Or(val c1: Contract, val c2: Contract) extends Contract
-    case class Union(val c1: Contract, val c2: Contract) extends Contract
     case object Succ extends Contract
     case object Fail extends Contract
 
@@ -46,46 +49,38 @@ object Language extends App {
             lazy val evalC2 = evalC (e) (c2)
             evalC1 match {
                 case Fail => evalC2
-                case _    => c1
+                case _    => evalC1
             }
         }
 
         def reduceSeq (c1: Contract, c2: Contract): Contract = {
             val resc1 = evalC (e) (c1)
-            resc1 match {
-                case Fail => Fail
-                case Succ => c2
-                case _ => resc1 followedBy c2 //Seq(resc1,c2)
-            }
-        }
-        
-        def reduceUnion (c1: Contract,c2: Contract) : Contract = {
-            val evalC1 = evalC (e) (c1)
-            val evalC2 = evalC (e) (c2)
-            (evalC1, evalC2) match {
-                    case (Fail, Fail)   => Fail
-                    case (Succ, Succ)   => Succ
-                    case (Fail, _)      => c1 && evalC2
-                    case (_, Fail)      => evalC1 && c2
-                    case (_,_)          => evalC1 && evalC2
+            (c1, resc1) match {
+                case (Fail, _) => Fail
+                case (Succ, _) => c2
+                case (_, Fail) => Fail
+                case (_, Succ) => c2
+                case _ => resc1 then c2 //Seq(resc1,c2)
             }
         }
 
+        def reduceCommitment (f: Transaction => Option[Contract], e: Transaction) = {
+            val o = f(e)
+            if (o.isDefined) o.get else Fail
+        }
+
         c match {
-            case Atom(f)        => if (f(e)) Succ else Fail
-            case Or(c1,c2)      => reduceOr (c1,c2)
-            case Seq(c1,c2)     => reduceSeq (c1,c2)
-            case Union(c1,c2)   => reduceUnion (c1,c2)
-            case Succ           => Succ
-            case Fail           => Fail // contract template can specify when it should fail
+            case Commitment(f)      => reduceCommitment (f, e)
+            case Or(c1,c2)          => reduceOr (c1,c2)
+            case Seq(c1,c2)         => reduceSeq (c1,c2)
+            case Succ | Fail        => Fail
         }
     }
 
     def prettyPrinting (c: Contract): Unit = c match {
-        case Atom(f)        => print(f)
+        case Commitment(f)    => print(c)
         case Or(c1,c2)      => print("OR: " + prettyPrinting(c1) + " is satisfied or " + prettyPrinting(c2) + " is satisfied\n")
-        case Seq(a,c)      => print("Seq: First " + prettyPrinting(a) + " must be satistied and then " + prettyPrinting(c) + " must be satified\n")
-        case Union(c1,c2)   => print("UNION: awaiting matching events in no particular order for " + prettyPrinting(c1) + " and " + prettyPrinting(c2) + "\n")
+        case Seq(a,c)       => print("Seq: First " + prettyPrinting(a) + " must be satistied and then " + prettyPrinting(c) + " must be satified\n")
         case Succ           => println("SUCCESS")
         case Fail           => println("BREACHED CONTRACT")
     }
